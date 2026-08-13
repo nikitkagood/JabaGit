cb4bda042d1beb46eaf9f6b5c691df970f5eb907
Nikita Belov <52581468+nikitkagood@users.noreply.github.com>
2026-08-13 18:37:14 +0300

    Homework 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 void main() {
+    //Homework 1
     Animal animal = new Animal("New animal name");
     animal.animalName.sayName();
 
     RecordAnimal recordAnimal = new RecordAnimal(new AnimalName("Record animal name"));
     recordAnimal.animalName().sayName();
+    //
+
 }
